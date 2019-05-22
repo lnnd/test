dd46d2bc0f0c61ba7c38ec58128e2466c3aecb65
Leonid Korotky <lnnd@ukr.net>
2019-05-22 21:28:40 +0300

    text

--- src/main/java/com/gittest/Main.java
+++ src/main/java/com/gittest/Main.java
@@ -2,9 +2,17 @@ package com.gittest;
 
 public class Main {
 
+    private String text;
+
     public static void main(String[] args) {
         System.out.println("master");
 
     }
 
+    @Override
+    public String toString() {
+        return "Main{" +
+                "text='" + text + '\'' +
+                '}';
+    }
 }
