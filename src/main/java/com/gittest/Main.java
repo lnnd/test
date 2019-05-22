70fe64352fba75e18d756fbd14dfdacbf8b3743b
Leonid Korotky <lnnd@ukr.net>
2019-05-22 21:28:40 +0300

    text
    (cherry picked from commit dd46d2bc0f0c61ba7c38ec58128e2466c3aecb65)

--- src/main/java/com/gittest/Main.java
+++ src/main/java/com/gittest/Main.java
@@ -5,11 +5,17 @@ public class Main {
     private int i;
     private int j;
 
+    private String text;
+
     public static void main(String[] args) {
         System.out.println("master");
 
     }
 
-
-
+    @Override
+    public String toString() {
+        return "Main{" +
+                "text='" + text + '\'' +
+                '}';
+    }
 }
