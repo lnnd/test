e58ad14e531c5ee7f18643ae0e290aeb843e82db
Leonid Korotky <lnnd@ukr.net>
2019-05-22 08:04:06 +0300

    master

--- src/main/java/com/gittest/Main.java
+++ src/main/java/com/gittest/Main.java
@@ -3,7 +3,7 @@ package com.gittest;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        System.out.println("master");
 
     }
 
