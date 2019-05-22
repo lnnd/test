f2ea9493bf897b4ca712a91f461cb927d833dfaa
Leonid Korotky <lnnd@ukr.net>
2019-05-22 21:39:40 +0300

    Revert "text"
    This reverts commit dd46d2bc

--- src/main/java/com/gittest/Main.java
+++ src/main/java/com/gittest/Main.java
@@ -2,25 +2,9 @@ package com.gittest;
 
 public class Main {
 
-    private String text;
-
     public static void main(String[] args) {
         System.out.println("master");
 
     }
 
-    @Override
-    public String toString() {
-        return "Main{" +
-                "text='" + text + '\'' +
-                '}';
-    }
-
-    public String getText() {
-        return text;
-    }
-
-    public void setText(String text) {
-        this.text = text;
-    }
 }
